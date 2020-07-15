12e65b2325ad66b643c164d59228954a23d9d43a
Ravi Joshi <ravipiyushjoshi@gmail.com>
2020-07-15 11:40:37 -0400

    Updated script for the vehicle rotation as well as move by the player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 20.0f;
+    public float speed = 10.0f;
+    public float turnSpeed= 25;
+    public float horizontalInput;
+    public float forwardInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
         // We will add code here to forawrd the  vehicle
         //transform.Translate(0, 0, 1);
 
@@ -22,7 +28,8 @@ public class PlayerController : MonoBehaviour
 
         // Slowing down a speed of a vehicle from above  function
 
-        transform.Translate(Vector3.forward * Time.deltaTime  * speed);
-
+        transform.Translate(Vector3.forward * Time.deltaTime  * speed * forwardInput);
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up, Time.deltaTime * horizontalInput * turnSpeed );
     }
 }
