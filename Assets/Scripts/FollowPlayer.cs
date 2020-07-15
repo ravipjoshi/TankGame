53d8a588bea37c8eda8634419df3806c0176b742
Ravi Joshi <ravipiyushjoshi@gmail.com>
2020-07-15 11:49:06 -0400

    Cleaned the code and add comments to the scripts

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
-    public Vector3 offset = new Vector3(0, 6, -8);
+    // public variable    
     public GameObject player;
+    //private variable
+    private Vector3 offset = new Vector3(0, 6, -8);
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +17,7 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //Offset Behind the player by adding to the player's position
-        //transform.position = player.transform.position + new Vector3 (0,5,-7);
-
+        
         //Offset Behind the player by adding to the player's position with vector3 variable
         transform.position = player.transform.position + offset;
     }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f;
-    public float turnSpeed= 25;
-    public float horizontalInput;
-    public float forwardInput;
+    //All private variables
+    private float speed = 10.0f;
+    private float turnSpeed= 25;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -18,18 +19,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Player Inputs
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        // We will add code here to forawrd the  vehicle
-        //transform.Translate(0, 0, 1);
-
-        // Forward a vehicle with vector3 which is shorthand style of above function
-        //transform.Translate(Vector3.forward);
-
-        // Slowing down a speed of a vehicle from above  function
-
+        // we move vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime  * speed * forwardInput);
-        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        // we can rotate vehicle
         transform.Rotate(Vector3.up, Time.deltaTime * horizontalInput * turnSpeed );
     }
 }
